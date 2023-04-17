b1480414bcab090426244b242dcbe8ad0f886697
Matt Rowan <matt.rowan121@gmail.com>
2023-04-17 12:55:44 -0400

    Add image to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,16 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <header className="flex justify-center items-center py-5 sticky top-0 bg-gray-400">
+    <header className="flex justify-between items-center p-5 sticky top-0 bg-gray-400">
       <Link href="/">
         <h1 className="text-3xl">Disney Made Easy</h1>
+      </Link>
+      <Link href="/">
+        <img 
+          src="/headshot.png"
+          alt="user-image" 
+          className="icon h-14 rounded-full cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"
+        />
       </Link>
     </header>
   )
