f714a8b77df5834be2ab2c907b37e792b42ec143
Matt Rowan <matt.rowan121@gmail.com>
2023-04-17 12:34:06 -0400

    Create initial buildout of attractions page

---
+++ src/pages/attractions.js
@@ -0,0 +1,16 @@
+import Header from '@/components/Header'
+import Head from 'next/head'
+
+export default function Attractions() {
+  return (
+    <div>
+      <Head>
+        <title>Disney Made Easy</title>
+        <meta name='description' content='Genereate Next App' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header/>
+      <h1>Hi</h1>
+    </div>
+  )
+}
