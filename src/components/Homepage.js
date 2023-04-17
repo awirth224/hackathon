b7d4b391b137f0f1adbbc736389da72489f470c2
Matt Rowan <matt.rowan121@gmail.com>
2023-04-17 12:12:23 -0400

    Change class to classname

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,7 +2,7 @@ const Homepage = () => {
   return (
     <div>
       <p>Disney Made Easy is an application that helps users plan and organize their trip to any Disney theme park or resort. It features interactive maps, itinerary builder, and wait time tracker to enhance the user's experience. Users can easily browse the map, plan their route, build personalized itinerary, and track wait times of attractions, shows, and dining reservations.</p>
-      <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">See All Attractions</button>
+      <button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">See All Attractions</button>
     </div>
   )
 }
