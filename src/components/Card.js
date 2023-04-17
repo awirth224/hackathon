95f2c2acb1aac53099a68e68216cf4e7ca590d36
Matt Rowan <matt.rowan121@gmail.com>
2023-04-17 12:46:32 -0400

    Create cards from array data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,33 @@
 import Image from "next/legacy/image";
+import disneyRides from '../../data';
+import { AiOutlineHeart } from 'react-icons/ai'
 
 const Card = () => {
-    return (
-        <div className="p-3 cursor-pointer xl:hover:scale-105 transition-transform duration-200 h-80 w-80">
-            <Image
-                src="https://images.unsplash.com/photo-1524008279394-3aed4643b30b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzbmV5fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-                alt="disney-logo"
-                height={100}
-                width={200}
-                // layout="responsive"
-            />
-            <div className="border">
-                <p>Ride Name</p>
-                <p>Ride Description</p>
-                <p>Ride Location</p>
+
+    const rideDetails = disneyRides.map((ride) => {
+        return(
+            <div className="p-3 cursor-pointer xl:hover:scale-105 transition-transform duration-200 h-80 w-80 border">
+                <Image
+                    src="https://images.unsplash.com/photo-1524008279394-3aed4643b30b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzbmV5fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                    alt="disney-logo"
+                    height={100}
+                    width={200}
+                    layout="responsive"
+                />
+                <div className="leading-8">
+                    <p>{ride.name}</p>
+                    <p>{ride.description}</p>
+                    <p>{ride.location}</p>
+                </div>
+                <AiOutlineHeart />
             </div>
-        </div>
+        )
+    })
+
+    return (
+        <>
+            {rideDetails}
+        </>
     )
 }
 
