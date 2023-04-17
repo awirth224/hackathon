fdcfc545c9a7363bcc9442ffae5ceedf1bd9ae5a
Matt Rowan <matt.rowan121@gmail.com>
2023-04-17 12:46:43 -0400

    Add container for cards

--- src/pages/attractions.js
+++ src/pages/attractions.js
@@ -1,3 +1,4 @@
+import Card from '@/components/Card'
 import Header from '@/components/Header'
 import Head from 'next/head'
 
@@ -10,7 +11,9 @@ export default function Attractions() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header/>
-      <h1>Hi</h1>
+      <div className="bg-gray-700 text-gray-200 p-5 gap-3 select-none sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+        <Card />
+      </div>
     </div>
   )
 }
