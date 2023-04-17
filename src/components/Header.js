dbc152290e11c070da6969f6e015256d6ba6490d
Matt Rowan <matt.rowan121@gmail.com>
2023-04-17 12:32:55 -0400

    Add link to home page from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 const Header = () => {
   return (
     <header className="flex justify-center items-center py-5 sticky top-0 bg-gray-400">
-      <h1 className="text-3xl">Disney Made Easy</h1>
+      <Link href="/">
+        <h1 className="text-3xl">Disney Made Easy</h1>
+      </Link>
     </header>
   )
 }
